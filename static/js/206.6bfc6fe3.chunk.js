943878aeb1a8b4dfce92202edc016bb5041f07dc
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2023-11-19 02:26:42 +0000

    Deploying to gh-pages from @ Sonata996/goit-react-hw-08-phonebook@ceb3bfef861d633fd5fcf6b5345c5185d83d0b53 🚀

---
+++ static/js/206.6bfc6fe3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[206],{1206:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var t=o(3329);function c(){return(0,t.jsx)("div",{children:(0,t.jsx)("h1",{children:"Welcome to your contact"})})}}}]);
+//# sourceMappingURL=206.6bfc6fe3.chunk.js.map
