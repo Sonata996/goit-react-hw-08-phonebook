1147374232dc90827f358444fe6f2448dfc9af2b
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2024-04-11 23:59:08 +0300

    fix ignore

--- frontend/build/static/js/153.7a347792.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[153],{6153:function(n,t,e){e.r(t),e.d(t,{default:function(){return u}});var o,r=e(168),c=e(6487).ZP.div(o||(o=(0,r.Z)(["\nmax-width: 420px;\nmargin-left: auto;\nmargin-right: auto;\n"]))),i=e(3329);function u(){return(0,i.jsx)(c,{children:(0,i.jsx)("h1",{children:"Welcome to your contact"})})}}}]);
-//# sourceMappingURL=153.7a347792.chunk.js.map
